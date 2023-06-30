1272274509f72af114bac807c4faa97a7c53efc1
Jos Paul <jospaul223@gmail.com>
2023-07-01 02:56:33 +0530

    updated one more functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,9 +4,16 @@ import { useState } from 'react'
 export default function TextForm(props) {
 
 
-  const handleClick = () =>{
+  const handleUpClick = () =>{
 
     let newText = text.toUpperCase();
+    setText(newText);
+
+  }
+
+  const handleLowClick = () =>{
+
+    let newText = text.toLowerCase();
     setText(newText);
 
   }
@@ -20,12 +27,21 @@ export default function TextForm(props) {
   const [text,setText] = useState('Enter the text');
 
   return (
+    <>
     <div>
         <h5>{props.heading}</h5>
         <div className = "mb-3">
             <textarea className="form-control" value = {text} onChange = {onChange} id="myBox" rows="5"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleClick}>convert to uppar case</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>convert to uppar case</button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>convert to lower case</button>
     </div>
+
+    <div className="container">
+      <h3>Your Text Summary!</h3>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} minutes to reac</p>
+      </div>
+    </>
   )
 }
