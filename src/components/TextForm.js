78f5a7e13775fba3091aab97630b2ced9bed96fc
Jos Paul <jospaul223@gmail.com>
2023-07-01 02:30:21 +0530

    added a basic functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function TextForm(props) {
+
+
+  const handleClick = () =>{
+
+    let newText = text.toUpperCase();
+    setText(newText);
+
+  }
+
+  const onChange = (event) =>{
+
+    console.log("onChange");
+    setText(event.target.value)
+  }
+
+  const [text,setText] = useState('Enter the text');
+
   return (
     <div>
-
         <h5>{props.heading}</h5>
         <div className = "mb-3">
-            <textarea className="form-control" id="myBox" rows="5"></textarea>
+            <textarea className="form-control" value = {text} onChange = {onChange} id="myBox" rows="5"></textarea>
         </div>
-        <button className="btn btn-primary">convert to uppar case</button>
+        <button className="btn btn-primary" onClick={handleClick}>convert to uppar case</button>
     </div>
   )
 }
